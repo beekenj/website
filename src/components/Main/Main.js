d750271755e6bfbb27161f402d60a7921684b621
Jack Beeken <beekenj@gmail.com>
2022-12-24 17:37:44 -0700

    Added some text

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,14 @@ import './Main.css';
 
 export default function Main(props) {
     return (
-        <h1>  
-          Hello github pages
-        </h1>
+        <>
+          <h1>  
+            My story
+          </h1>
+          <br />
+          <p>
+            I am a software developer with an interest in design and a passion for data. I have bachelor degrees in both Computer Science and Philosophy.
+          </p>
+        </>
     )
 }

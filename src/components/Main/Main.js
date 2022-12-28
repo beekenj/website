3c26b8264b9a0731f3f40ea0e69945612051f31f
Jack Beeken <beekenj@gmail.com>
2022-12-27 21:40:18 -0700

    reformat

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,12 +3,12 @@ import './Main.css';
 export default function Main(props) {
     return (
         <>
-          <h1>  
-            My story
-          </h1>
-          <br />
           <p>
-            I am a software developer with an interest in design and a passion for data. I have bachelor degrees in both Computer Science and Philosophy.
+            <h1>  
+              My story
+            </h1>
+            <br />
+              I am a software developer with an interest in design and a passion for data. I have bachelor degrees in both Computer Science and Philosophy.
           </p>
         </>
     )
